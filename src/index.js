be0537502ce957e48d0a83157767de2b64faa111
Mariano Demaria <demaria.mariano@gmail.com>
2022-07-08 01:04:06 -0300

    Added mqtt client test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const dotenv = require('dotenv')
 dotenv.config()
+
 const cfg = require('./config')
 const app = express()
+
+require('./mqtt')
+
 app.listen(cfg.PORT, () => console.log(`Running at http://localhost:${cfg.PORT}`))
